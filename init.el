2d9958d228cb2773dd88f12c41238c5769bbe1e0
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-12-11 13:01:15 -0300

    fix load-path duplicate

--- init.el
+++ init.el
@@ -599,7 +599,7 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
-  (add-to-list 'load-path "~/.spacemacs.d/user-init.el")
+  (add-to-list 'load-path "~/.spacemacs.d/user-config.el")
 )
 
 
