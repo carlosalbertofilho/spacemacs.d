748a55769c3ddff3f6493892b6b67631a1bd6b7a
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-12-16 22:53:56 -0300

    fix load-file for layers.el position

--- init.el
+++ init.el
@@ -91,7 +91,7 @@ This function should only modify configuration layer settings."
    ;; `dotspacemacs/user-config'. To use a local version of a package, use the
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '((load-file "~/.spacemacs.d/layers.el"))
+   dotspacemacs-additional-packages '()
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -588,6 +588,7 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
 This function is called only while dumping Spacemacs configuration. You can
 `require' or `load' the libraries of your choice that will be included in the
 dump."
+  (load-file "~/.spacemacs.d/layers.el")
 )
 
 
